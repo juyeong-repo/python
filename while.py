779fefdbfb92137bc850cbe5607311a646bb241c
juyeongpark <juyeongpark@coredax.com>
2022-12-16 20:55:25 +0900

    while

---
+++ while.py
@@ -0,0 +1,18 @@
+# 반복문 while
+
+guest = 1
+while guest < 10:
+    guest = guest + 1
+    print("손님이 {}명 입니다.".format(guest))
+    if guest == 10 :
+        print ("손님이 꽉 찼습니다.")
+        
+        
+guest = 1
+while guest < 10:
+    guest = guest + 1
+    print("손님이 {}명 입니다.".format(guest))
+    if guest == 10 :
+        print ("손님이 꽉 찼습니다.")
+        
+        
