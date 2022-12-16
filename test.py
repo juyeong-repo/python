d01f96d3150aa8340dba2114f17c330dab2aeb1b
juyeongpark <juyeongpark@coredax.com>
2022-12-16 16:28:15 +0900

    list

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
-print("Hello, world!")
+#list
+
+a= []
+type (a)
+print (a) 
+
+# 문자열처럼 인덱싱 / 슬라이싱 가능 (결과: 리스트)
+
+# append() 리스트 추가
+# insert() 리스트 삽입
+# del() 인덱스 삭제
+# remove(요소) 요소 삭제 
+# pop(인덱스) 삭제 및 값 리턴
+# extend() 리스트 확장
+# sort() 정렬
+# in 포함여부 true/false
+# reverse() 역정렬
+# dir(list)하면 메소드 모두 볼 수 있음! 
+
+
+
+
+ 
+
